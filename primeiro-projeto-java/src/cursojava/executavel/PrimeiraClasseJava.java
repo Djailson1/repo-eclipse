81ee4e50615a5ec5a9b77f92a991e61cf655ae53
Djailson1 <djailson_silva1@outlook.com>
2022-05-12 22:10:49 -0300

    alteraçoes no commit

--- primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,10 +1,11 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
-	/*Main é um método autoexecutável em java*/
+	/*Main é um método autoexecutável em javadsdsd*/
 	public static void main(String[] args) {
 		/*Escreve texto no console*/
 		System.out.println("testando commits no eclipse");
+		System.out.println("olá, mundo!");
 	}
 
 }
