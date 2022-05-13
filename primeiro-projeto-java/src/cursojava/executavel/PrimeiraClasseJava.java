dbde5e19ab6104e5475699e8f243b044e9aad97c
Djailson1 <djailson_silva1@outlook.com>
2022-05-12 21:25:21 -0300

    criação do projeto

---
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*Main é um método autoexecutável em java*/
+	public static void main(String[] args) {
+		/*Escreve texto no console*/
+		System.out.println("testando commits no eclipse");
+	}
+
+}
